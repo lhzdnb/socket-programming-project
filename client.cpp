12a83d523751f988430c0bbad25e4740ab737e6f
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-16 01:09:49 -0800

    Add some header file

--- client.cpp
+++ client.cpp
@@ -13,6 +13,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "build_encryption_map.h"
 
 using namespace std;
